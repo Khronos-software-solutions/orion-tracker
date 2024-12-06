a0888d4118e36baf42f6f8db882fc98e7c2997da
definitely-not-a-furry <heijnisties@outlook.com>
2024-12-06 15:38:28 +0100

    init

---
+++ extended_module.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+class XMReader:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.header: dict[str, Any] = {}
+        self.patterns: list[dict[str, Any]] = []
+        self.samples: list[dict[str, Any]] = []
+
+    def load_file(self):
+        self.read_header(self.file_path)
+
+    def read_header(self, file: str):
+        with open(file, 'rb') as f:
+            id_text = f.read(17).decode('ascii')
+            name = f.read(20).decode('ascii').strip()
+            null_terminator = f.read(1)
+            tracker_name = f.read(20).decode('ascii').strip()
+            version = f.read(2)
+            header_size = f.read(2)
+            song_length = f.read(2)
+            song_restart = f.read(2)
+            channel_number = int.from_bytes(f.read(2))
+            pattern_number = int.from_bytes(f.read(2))
+            instrument_number = int.from_bytes(f.read(2))
+            self.header.update({
+                'id_text': id_text,
+                'name': name,
+                'null_terminator': null_terminator,
+                'tracker_name': tracker_name,
+                'version': version,
+                'header_size': header_size,
+                'song_length': song_length,
+                'song_restart': song_restart,
+                'channel_number': channel_number,
+                'pattern_number': pattern_number,
+                'instrument_number': instrument_number
+            })
+        print(self.header)
---
+++ main.py
@@ -0,0 +1,4 @@
+import extended_module
+
+reader = extended_module.XMReader('./amblight.xm')
+reader.load_file()
