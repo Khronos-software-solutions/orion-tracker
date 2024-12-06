3c7f0046d1b688b6539536382040de68efaa7305
Lukycrafter3 <luuk.kalkman@yahoo.com>
2024-12-06 16:11:03 +0100

    things

---
+++ ui.py
@@ -0,0 +1,9 @@
+import customtkinter as tk
+
+class App(tk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title('Orion-tracker')
+        self.geometry('300x200')
+        
+
