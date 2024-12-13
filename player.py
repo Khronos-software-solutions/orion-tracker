df5611122b547c0d9c34f9300760e3ddc819d3e5
definitely-not-a-furry <heijnisties@outlook.com>
2024-12-13 16:10:04 +0100

    Sound parsing actually works now

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ from module import Pattern
 if __name__ == "__main__":
     r = XMReader('./amb-nrg.xm')
     r.load_file()
-    s = r.instruments[0]['samples'][0]
+    s = r.instruments[5]['samples'][0]
     bit_depth = (int.from_bytes(s['type']) >> 4) & 1
     data: bytes = s['data']
     length = s['length']
@@ -21,17 +21,13 @@ if __name__ == "__main__":
 
     for i in range(length):
         old += struct.unpack('<b', d.read(1))[0]
-        print(old)
         real_sample += old.to_bytes(2, signed=True)
 
-    print(bit_depth)
     if bit_depth == 0:
         bit_depth = 1
     else:
         bit_depth = 2
-        
-    print(s['length'])
-    print(real_sample)
-    with wave.open('./sample1.wav', 'wb') as f:
-        f.setparams((1, 2, 42200, 0, 'NONE', 'NONE'))
+
+    with wave.open('./sample5.wav', 'wb') as f:
+        f.setparams((1, 2, 8500, 0, 'NONE', 'NONE'))
         f.writeframes(real_sample)
